05b38c9a9035998e9dbeb24f186707b6c7c48b05
LAPTOP-DMIU0VGE\Evianaive <153540933@qq.com>
2021-10-15 10:10:56 +0800

    2021 1015 10:10 增加UPMSGraphNode类

---
+++ Source/PMS/Public/PMSGraphNode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "PMSGraphNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PMS_API UPMSGraphNode : public UObject
+{
+	GENERATED_BODY()
+	
+};
